28ba8dc344f85560565a653666f7e76ed26720cf
Bhadri01 <bhadrinathan28@gmail.com>
2025-03-24 14:27:03 +0000

    refactor: QueryParams to simplify filter and sort descriptions in params.py

--- fastapi_filter_sort/params.py
+++ fastapi_filter_sort/params.py
@@ -7,8 +7,8 @@ from fastapi import Query
 class QueryParams:
     def __init__(
         self,
-        filters: Optional[str] = Query(None,description="A JSON string representing filter conditions.",alias="filters",example='{"field": {"condition":"value"}} or {"$and":[{"field": {"condition":"value"}}]}'),
-        sort: Optional[str] = Query(None,description="e.g. name:asc or user__email:desc", example="name:asc"),
+        filters: Optional[str] = Query(None,description="A JSON string representing filter conditions."),
+        sort: Optional[str] = Query(None,description="e.g. name:asc or user__email:desc"),
         search: Optional[str] = Query(None,description="A string for global search across string fields.")
     ):
         self.filters = filters
